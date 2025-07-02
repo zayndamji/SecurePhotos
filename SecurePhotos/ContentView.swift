cc715374bcf5d51171045ee1873741e56c1409ca
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-07-02 16:03:29 -0700

    moved file entry list to seperate view

--- SecurePhotos/ContentView.swift
+++ SecurePhotos/ContentView.swift
@@ -3,68 +3,10 @@ import SwiftData
 import UniformTypeIdentifiers
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
     @Query private var entries: [FileEntry]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(entries) { entry in
-                    NavigationLink {
-                        FileEntryView(url: entry.url)
-                    } label: {
-                        Text(entry.timeCreated, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-            let url = documents.appendingPathComponent(
-                UUID().uuidString + ".txt")
-            
-            do {
-                try "Hello World".data(using: .utf8)?.write(to: url)
-                print("File created")
-            } catch {
-                print("Error")
-            }
-
-            let entry = FileEntry(timeCreated: Date(), fileType: UTType.text, url: url)
-            
-            modelContext.insert(entry)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                do {
-                    try FileManager.default.removeItem(at: entries[index].url)
-                    print("File deleted")
-                } catch {
-                    print("Error")
-                }
-            
-                modelContext.delete(entries[index])
-            }
-        }
+        FileEntryListView(entries: entries)
     }
 }
 
